13756c7b239df263cfc31196766d62daf0b65e7b
sammosummo <samuel.mathias@fastmail.com>
2023-04-25 11:07:08 -0400

    added sumary field to Experiment

--- expflow/expflow.py
+++ expflow/expflow.py
@@ -979,6 +979,7 @@ class Experiment(_SerialisationMixin, _StatusMixin, _ExpReqFieldsMixin):
 
     trial_index: int | None = field(default=None, **kw)
     trials: list[Trial] = field(default_factory=list, **kw)
+    summary: dict[str, int | float] | None = field(default=None, **kw)
 
     def __post_init__(self):
         super().__post_init__()
